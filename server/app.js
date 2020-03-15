06766d35980454bda97b33b7e37326fb69a04514
Trin Sengchareon <melonn.qq@gmail.com>
2020-03-15 15:19:16 +0700

    add server dir and app.js

---
+++ server/app.js
@@ -0,0 +1,17 @@
+// require library
+const express = require('express');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const app = express();
+
+// declare constant values
+const PORT = process.env.ports || 8001;
+
+// setup middleware
+app.use(morgan('dev'));
+app.use(errorhandler());
+
+// start server
+app.listen(PORT, () => {
+    console.log(`server starts on port ${PORT}`);
+})
