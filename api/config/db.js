78935d0d4d337b5a1cbaa9ba9304cb896a16a397
Trin Sengchareon <melonn.qq@gmail.com>
2020-03-21 21:55:42 +0700

    add mysql connection in controller

---
+++ api/config/db.js
@@ -0,0 +1,11 @@
+const config = {
+    setup: {
+        host     : 'localhost',
+        user     : 'root',
+        password : 'pwd',
+        database : 'promptclaim',
+        port: 8083
+    }
+};
+
+module.exports = config;
--- api/controllers/test-controller.js
+++ api/controllers/test-controller.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mysql = require('mysql');
+const config = require('../config/db');
+
+// connect to mysql
+const db = mysql.createConnection(config.setup);
+db.connect((err) => {
+    if (err) throw err;
+    console.log('mysql connected as id ' + db.threadId);
+});
 
 // business logic prepared for routes
 const hello = (req, res, next) => {
