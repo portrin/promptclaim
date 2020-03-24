2a164a91f79c6b24498c3d70c88e1918232717e6
teeteerat <teerat.boo@gmail.com>
2020-03-24 17:56:29 +0700

    correct product-model

--- api/models/product-model.js
+++ api/models/product-model.js
@@ -1,7 +1,9 @@
+const db = require('../config/db');
+
 module.exports = class Product {
-    constructor(serialNo, number, model, description, category, status, price, supplier) {
+    constructor(serialNo, productNo, model, description, category, status, price, supplier) {
         this.serialNo = serialNo;
-        this.number = number;
+        this.productNo = productNo;
         this.model = model;
         this.description = description;
         this.category = category;
@@ -12,30 +14,33 @@ module.exports = class Product {
 
     save() {
         return db.execute(
-            'INSERT INTO products (serialNo, number, model, description, category, status, price, supplier) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-            [this.serialNo = serialNo,
-            this.number = number,
-            this.model = model,
-            this.description = description,
-            this.category = category,
-            this.status = status,
-            this.price = price,
-            this.supplier = supplier]
+            'INSERT INTO products (serial_no, product_no, product_model, product_description, product_category, status, price, supplier_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [this.serialNo, 
+            this.productNo,
+            this.model,
+            this.description,
+            this.category,
+            this.status,
+            this.price,
+            this.supplier]
         );
     }
 
-    static deleteById(id) {
-        //DELETE
+    static deleteById(serialNo, productNo) {
+        return db.execute(
+            'DELETE FROM products WHERE products.serial_no = ? AND products.product_no = ?',
+            [serialNo, productNo]
+        );
     }
 
     static fetchAll() {
         return db.execute('SELECT * FROM products');
     }
 
-    static findById(id, cb) {
+    static findById(serialNo, productNo, cb) {
         return db.execute(
-            'SELECT * FROM products WHERE products.id = ?',
-            [id]
+            'SELECT * FROM products WHERE products.serial_no = ? AND products.product_no = ?',
+            [serialNo, productNo]
         );
     }
 }
