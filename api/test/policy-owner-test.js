c713c53b6220ee5ca7f413fec335621a29ff1e94
Trin Sengchareon <melonn.qq@gmail.com>
2020-04-07 17:55:28 +0700

    refactor policy owner

--- api/test/policy-owner-test.js
+++ api/test/policy-owner-test.js
@@ -2,7 +2,7 @@ const Customer = require('../models/customer/customer-model');
 const PurchasedProduct = require('../models/product/purchased-product-model');
 
 const customer = new Customer('123', 'Trin', 'seng', '123432', '1999', 'M');
-console.log(new PurchasedProduct('','','','','','','','','','','','','','','').constructor.name)
+
 customer.addPurchasedProduct(new PurchasedProduct('','','','','','','','','','','','','','',''));
 
 
