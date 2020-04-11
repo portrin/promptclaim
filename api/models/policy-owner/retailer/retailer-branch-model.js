e0a307fd944f78f6efad0ab95fd6011161e1b6e9
teeteerat <teerat.boo@gmail.com>
2020-04-11 16:29:57 +0700

    create getter and setter for policy and policy owner packages

--- api/models/policy-owner/retailer/retailer-branch-model.js
+++ api/models/policy-owner/retailer/retailer-branch-model.js
@@ -1,35 +1,72 @@
 const Retailer = require('./retailer-model');
+const checkType = require('../../utils').checkType;
 
 module.exports = class RetailerBranch {
-    constructor(branchName, contact, address) {
+    constructor({ branchName = null, contact = null, address = null } = {}) {
         // their own class atrribute ref. from class diagram
         this._branchId = branchId;
         this._branchName = branchName;
         this._contact = contact;
         this._address = address;
         // their relationships to its neighbor ref. from class diagram
-        this._retailer;
-        this._purchasedProduct = [];
+        this._retailer = null;        // relationship to Retailer
+        this._purchasedProduct = [];  // relationship to PurchasedProduct
     }
 
     // DM layer CRUD
-    _create () {
+    _create() {
         return db.execute('INSERT INTO retailer_branch(retailer_id, branch_id, branch_name, contact, address) VALUES (?,?,?,?,?)',
-            [/*retailerId*/, this._branchId, this._branchName, this._contact, this._address]
+            [this._retailer.getProperty.retailerId, this._branchId, this._branchName, this._contact, this._address]
         );
     }
 
-    static _read () {
+    static _read() {
         return db.execute('SELECT * FROM retailer_branch');
     }
 
-    _update () {
+    static _readByPk(retailerId, branchId) {
+        return db.execute('SELECT * FROM retailer_branch WHERE retailer_id = ? AND branch_id = ?', [retailerId, branchId]);
+    }
+
+    _update() {
         return db.execute('UPDATE retailer_branch SET branch_name = ?, contact = ?, address = ? WHERE retailer_id = ? AND branch_id = ?',
-            [this._branchName, this._contact, this._address, /*retailerId*/, this._branchId]
+            [this._branchName, this._contact, this._address, this._retailer.getProperty.retailerId, this._branchId]
         );
     }
 
-    _delete () {
-        return db.execute('DELETE FROM retailer_branch WHERE retailer_id = ? AND branch_id = ?', [/*retailerId*/, this._branchId]);
+    _delete() {
+        return db.execute('DELETE FROM retailer_branch WHERE retailer_id = ? AND branch_id = ?', [this._retailer.getProperty.retailerId, this._branchId]);
     }
+
+    // getter and setter
+    getProperty() {
+        return {
+            branchId = this._branchId,
+            branchName = this._branchName,
+            contact = this._contact,
+            address = this._address,
+            retailer = this._retailer,
+            purchasedProduct = this._purchasedProduct
+        };
+    }
+
+    setProperty({ // set only its own attributes
+        // destructuring object as parameter by using old values as a default.
+        branchId = this._branchId,
+        branchName = this._branchName,
+        contact = this._contact,
+        address = this._address
+    }) {
+        // check datatype
+        checkType(branchId, 'String');
+        checkType(branchName, 'String');
+        checkType(contact, 'String');
+        checkType(address, 'String');
+        // assign to private variables
+        this._branchId = branchId;
+        this._branchName = branchName;
+        this._contact = contact;
+        this._address = address;
+    }
+
 }
--- api/models/service-center/service-center-model.js
+++ api/models/service-center/service-center-model.js
@@ -2,14 +2,14 @@ const db = require('../../config/db');
 const checkType = require('../../utils').checkType;
 
 module.exports = class ServiceCenter {
-    constructor(serviceCenterId, name, serviceCenterDescription, hqAddress) {
+    constructor({ serviceCenterId = null, name = null, serviceCenterDescription = null, hqAddress = null } = {}) {
         // their own class atrribute ref. from class diagram
         this._serviceCenterId = serviceCenterId;
         this._name = name;
         this._serviceCenterDescription = serviceCenterDescription;
         this._hqAddress = hqAddress;
         // their relationships to its neighbor ref. from class diagram
-        this._serviceCenterBranch = [];
+        this._serviceCenterBranch = []; // relationship to ServiceCenterBranch
     }
 
     // DM layer CRUD
@@ -25,12 +25,46 @@ module.exports = class ServiceCenter {
         return db.execute('SELECT * FROM service_center');
     }
 
+    static _readByServiceCenterId(serviceCenterId) {
+        return db.execute('SELECT * FROM service_center WHERE service_center_id = ?', [serviceCenterId]);
+    }
+
     _update() {
-        return db.execute('UPDATE service_center SET name = ?, hq_address = ?, service_center_description= ? WHERE service_center_id = ?', 
-        [this._name, this._hqAddress, this._serviceCenterDescription, this._serviceCenterId]);
+        return db.execute('UPDATE service_center SET name = ?, hq_address = ?, service_center_description= ? WHERE service_center_id = ?',
+            [this._name, this._hqAddress, this._serviceCenterDescription, this._serviceCenterId]);
     }
 
     _delete() {
         return db.execute('DELETE FROM service_center WHERE service_center_id = ?', [this._serviceCenterId]);
+    }
+
+    // getter and setter
+    getProperty() {
+        return {
+            serviceCenterId = this._serviceCenterId,
+            name = this._name,
+            serviceCenterDesciption = this._serviceCenterDescription,
+            hqAddress = this._hqAddress,
+            serviceCenterBranch = this._serviceCenterBranch
+        };
+    }
+
+    setProperty({ // set only its own attributes
+        // destructuring object as parameter by using old values as a default.
+        serviceCenterId = this._serviceCenterId,
+        name = this._name,
+        serviceCenterDesciption = this._serviceCenterDescription,
+        hqAddress = this._hqAddress
+    }) {
+        // check datatype
+        checkType(serviceCenterId, 'String');
+        checkType(name, 'String');
+        checkType(serviceCenterDescription, 'String');
+        checkType(hqAddress, 'String');
+        // assign to private variables
+        this._serviceCenterId = serviceCenterId;
+        this._name = name;
+        this._serviceCenterDescription = serviceCenterDesciption;
+        this._hqAddress = hqAddress;
     }
 }
