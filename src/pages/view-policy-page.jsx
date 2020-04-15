c9ded11656d49b8113a7e83f0ffff4280b19e377
AlphaZ <kan1657@gmail.com>
2020-04-16 00:58:32 +0700

    Clean import non-used from Lieng refactor(View-PolicyPage)

--- src/pages/view-policy-page.jsx
+++ src/pages/view-policy-page.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { Layout, Breadcrumb, Menu, Descriptions, Collapse } from 'antd'
+import { Descriptions, Collapse } from 'antd'
 import { AppLayout } from '../components/app-layout'
-
-import { Topbar } from '../components/header'
-import { Bottombar } from '../components/footer'
-
-const { SubMenu } = Menu
 
 const { Panel } = Collapse
 
@@ -28,124 +23,117 @@ export const ViewPolicyPage = (props) => {
   return (
     <AppLayout {...props} nosearch title="Product Information">
       <div className="site-layout-content">
-                  <Descriptions layout="vertical" bordered>
-                    <Descriptions.Item label="Policy Informaion">
-                      Policy ID: 000037
-                      <br />
-                      <Collapse>
-                        <Panel header="Policy Description">
-                          <p>{text}</p>
-                        </Panel>
-                      </Collapse>
-                    </Descriptions.Item>
-                  </Descriptions>
-                  <Descriptions layout="vertical" bordered>
-                    <Descriptions.Item label="Product Information">
-                      <Collapse onChange={callback}>
-                        <Panel
-                          header="AMERICAN STANDARD Automatic Toilet Bowl"
-                          key="1"
-                        >
-                          <Collapse>
-                            <Panel header="Customer 1" key="1">
-                              Customer ID: 000293
-                              <br />
-                              Product Number: 000001
-                              <br />
-                              Serial Number: 000056
-                              <br />
-                              Policy Period: 10/3/20 to 10/3/22
-                            </Panel>
-                            <Panel header="Customer 2" key="2">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                            <Panel header="Customer 3" key="3">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                          </Collapse>
-                        </Panel>
-                        <Panel header="AMERICAN STANDARD Toilet Bowl" key="2">
-                          <Collapse>
-                            <Panel header="Customer 1" key="1">
-                              Customer ID: 000293
-                              <br />
-                              Product Number: 000001
-                              <br />
-                              Serial Number: 000056
-                              <br />
-                              Policy Period: 10/3/20 to 10/3/22
-                            </Panel>
-                            <Panel header="Customer 2" key="2">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                            <Panel header="Customer 3" key="3">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                          </Collapse>
-                        </Panel>
-                        <Panel header="AMERICAN STANDARD Water Heater" key="3">
-                          <Collapse>
-                            <Panel header="Customer 1" key="1">
-                              Customer ID: 000293
-                              <br />
-                              Product Number: 000001
-                              <br />
-                              Serial Number: 000056
-                              <br />
-                              Policy Period: 10/3/20 to 10/3/22
-                            </Panel>
-                            <Panel header="Customer 2" key="2">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                            <Panel header="Customer 3" key="3">
-                              Customer ID: 000353
-                              <br />
-                              Product Number: 000005
-                              <br />
-                              Serial Number: 000012
-                              <br />
-                              Policy Period: 14/4/19 to 14/4/21
-                            </Panel>
-                          </Collapse>
-                        </Panel>
-                      </Collapse>
-                    </Descriptions.Item>
-                  </Descriptions>
-                </div>
+        <Descriptions layout="vertical" bordered>
+          <Descriptions.Item label="Policy Informaion">
+            Policy ID: 000037
+            <br />
+            <Collapse>
+              <Panel header="Policy Description">
+                <p>{text}</p>
+              </Panel>
+            </Collapse>
+          </Descriptions.Item>
+        </Descriptions>
+        <Descriptions layout="vertical" bordered>
+          <Descriptions.Item label="Product Information">
+            <Collapse onChange={callback}>
+              <Panel header="AMERICAN STANDARD Automatic Toilet Bowl" key="1">
+                <Collapse>
+                  <Panel header="Customer 1" key="1">
+                    Customer ID: 000293
+                    <br />
+                    Product Number: 000001
+                    <br />
+                    Serial Number: 000056
+                    <br />
+                    Policy Period: 10/3/20 to 10/3/22
+                  </Panel>
+                  <Panel header="Customer 2" key="2">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                  <Panel header="Customer 3" key="3">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                </Collapse>
+              </Panel>
+              <Panel header="AMERICAN STANDARD Toilet Bowl" key="2">
+                <Collapse>
+                  <Panel header="Customer 1" key="1">
+                    Customer ID: 000293
+                    <br />
+                    Product Number: 000001
+                    <br />
+                    Serial Number: 000056
+                    <br />
+                    Policy Period: 10/3/20 to 10/3/22
+                  </Panel>
+                  <Panel header="Customer 2" key="2">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                  <Panel header="Customer 3" key="3">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                </Collapse>
+              </Panel>
+              <Panel header="AMERICAN STANDARD Water Heater" key="3">
+                <Collapse>
+                  <Panel header="Customer 1" key="1">
+                    Customer ID: 000293
+                    <br />
+                    Product Number: 000001
+                    <br />
+                    Serial Number: 000056
+                    <br />
+                    Policy Period: 10/3/20 to 10/3/22
+                  </Panel>
+                  <Panel header="Customer 2" key="2">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                  <Panel header="Customer 3" key="3">
+                    Customer ID: 000353
+                    <br />
+                    Product Number: 000005
+                    <br />
+                    Serial Number: 000012
+                    <br />
+                    Policy Period: 14/4/19 to 14/4/21
+                  </Panel>
+                </Collapse>
+              </Panel>
+            </Collapse>
+          </Descriptions.Item>
+        </Descriptions>
+      </div>
     </AppLayout>
   )
 }
-
-const { Content, Sider } = Layout
-
-
