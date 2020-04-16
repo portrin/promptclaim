6ae51be8c4b648f3a3368279ce38d2b6f37009eb
Sarut Sakulwira <sarutsakulwira@Saruts-MacBook-Pro.local>
2020-04-16 21:57:56 +0700

    fetch api to view product page

--- src/pages/view-product-page.jsx
+++ src/pages/view-product-page.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React , { useState, useEffect }from 'react'
 import { useParams } from 'react-router-dom'
 import { Descriptions } from 'antd'
 import { AppLayout } from '../components/app-layout'
 
 export const ViewProductPage = (props) => {
   let { key } = useParams()
+  //API Breaking Bad
+  const [item, setItems] = useState([])
+  const fetchItem = async () => {
+    const data = await fetch(`https://www.breakingbadapi.com/api/characters/`+key)
+    const item = await data.json()
+    setItems(item)
+    console.log(item)
+  }
+  useEffect(() => {
+    fetchItem()
+  }, [])
   return (
     <AppLayout {...props} nosearch title="Product Information">
       <div className="site-layout-content">
         <Descriptions
-          title={data[key].productName}
+          title=
+          {item.map((item) => (
+            item.name
+          ))}
           layout="vertical"
           bordered
           column={2}
@@ -17,7 +31,11 @@ export const ViewProductPage = (props) => {
           <Descriptions.Item label="Product Image">
             <img
               className="product-image"
-              src={data[key].image}
+              //src={data[key].image}
+              src=
+              {item.map((item) => (
+                item.img
+              ))}
               alt="product"
             />
           </Descriptions.Item>
@@ -93,6 +111,27 @@ const data = {
     image: '/ame-toilet.jpg',
 
     productName: 'AMERICAN STANDARD Toilet Bowl',
+
+    productInfo: {
+      purchaseDate: '20/03/2019',
+      serialNum: '000001',
+      warrantyNum: '000001',
+      supplierID: '000001',
+      retailerID: '000001',
+    },
+    customerInfo: {
+      customerID: '123000',
+      customerName: 'Jesse Pinkman',
+      phoneNum: '0913332211',
+      birthDate: '17/08/1998',
+      address: '35 phayathai 10',
+    },
+    claimInfo: {
+      expiryDate: '1/1/1',
+      claimPeriod: '2 years',
+      claimLeft: '1 year',
+      status: 'Unclaim',
+    },
   },
   '3': {
     key: '3',
@@ -100,6 +139,27 @@ const data = {
     image: '/water-heater.jpg',
 
     productName: 'STIEBEL ELTRON Water Heater',
+
+    productInfo: {
+      purchaseDate: '20/03/2019',
+      serialNum: '000001',
+      warrantyNum: '000001',
+      supplierID: '000001',
+      retailerID: '000001',
+    },
+    customerInfo: {
+      customerID: '123000',
+      customerName: 'Jesse Pinkman',
+      phoneNum: '0913332211',
+      birthDate: '17/08/1998',
+      address: '35 phayathai 10',
+    },
+    claimInfo: {
+      expiryDate: '1/1/1',
+      claimPeriod: '2 years',
+      claimLeft: '1 year',
+      status: 'Unclaim',
+    },
   },
   '4': {
     key: '4',
@@ -107,5 +167,26 @@ const data = {
     image: '/air.jpg',
 
     productName: 'DAIKIN Air Conditioner',
+
+    productInfo: {
+      purchaseDate: '20/03/2019',
+      serialNum: '000001',
+      warrantyNum: '000001',
+      supplierID: '000001',
+      retailerID: '000001',
+    },
+    customerInfo: {
+      customerID: '123000',
+      customerName: 'Jesse Pinkman',
+      phoneNum: '0913332211',
+      birthDate: '17/08/1998',
+      address: '35 phayathai 10',
+    },
+    claimInfo: {
+      expiryDate: '1/1/1',
+      claimPeriod: '2 years',
+      claimLeft: '1 year',
+      status: 'Unclaim',
+    },
   },
 }
