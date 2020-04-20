30cac56980d0e715673fc57f9b1966b89d6fdb32
AlphaZ <kan1657@gmail.com>
2020-04-20 23:00:07 +0700

    Add Dropdown to User logo in layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu, Dropdown } from 'antd'
+
 import userlogo from '../photo/user.png'
 import logo from '../photo/logo.png'
 
@@ -8,12 +9,15 @@ const { Header } = Layout
 export const Topbar = () => {
   return (
     <Header className="header">
-      <img
-        className="user"
-        src={userlogo}
-        alt="user logo"
-        style={{ float: 'right' }}
-      />
+      <Dropdown className="pointer" overlay={menu} trigger={['click']}>
+        <img
+          className="user"
+          src={userlogo}
+          alt="user logo"
+          style={{ float: 'right' }}
+        />
+      </Dropdown>
+
       <img
         className="logo"
         src={logo}
@@ -23,3 +27,17 @@ export const Topbar = () => {
     </Header>
   )
 }
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a href="http://www.alipay.com/">1st menu item</a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a href="http://www.taobao.com/">2nd menu item</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="2">
+      <a href="/login">LogOut</a>
+    </Menu.Item>
+  </Menu>
+)
