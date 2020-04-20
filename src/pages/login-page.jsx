f94adedf0526681927b9fb957ee1df65ba664709
AlphaZ <kan1657@gmail.com>
2020-04-20 21:38:54 +0700

    Fix Token Bug Woi!!!!

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -24,6 +24,7 @@ export const LoginPage = (props) => {
           username: username,
           password: password,
         },
+
         {
           headers: {
             'Content-Type': 'application/json',
