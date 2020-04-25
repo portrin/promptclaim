be2420584ab7f43272b9fa593628f10ee815dc5f
Som D <som@Soms-MacBook-Pro.local>
2020-04-25 23:03:44 +0700

    fix sort

--- src/pages/MyWarranty.tsx
+++ src/pages/MyWarranty.tsx
@@ -87,8 +87,8 @@ const MyWarranty: React.FC<Productprops> = () => {
       );
     } else if (sortBy === "Name Z-A") {
       return item.sort().reverse();
-    } else if (sortBy === "Product ID") {
-      return item.sort((a, b) => parseInt(a.uuid) - parseInt(b.uuid));
+    } else if (sortBy === "Retailer") {
+      return item.sort((a, b) => parseInt(a.retailer_branch_name) - parseInt(b.retailer_branch_name));
     } else {
       return item;
     }
@@ -208,8 +208,8 @@ const MyWarranty: React.FC<Productprops> = () => {
                     <IonSelectOption value="Name Z-A">
                       By name Z-A{" "}
                     </IonSelectOption>
-                    <IonSelectOption value="Name Z-A">
-                      Name Z-A{" "}
+                    <IonSelectOption value="Retailer">
+                      Retailer Name{" "}
                     </IonSelectOption>
                   </IonSelect>
                 </IonButton>
