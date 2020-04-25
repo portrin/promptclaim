cf986764b67b22784b27356f715e19d30f09074f
Som D <som@Soms-MacBook-Pro.local>
2020-04-25 23:06:49 +0700

    Remove policy btn

--- src/pages/DynamicWarrantyInfo.tsx
+++ src/pages/DynamicWarrantyInfo.tsx
@@ -291,13 +291,7 @@ const WarrantyInfo: React.FC<Match> = ({ match }) => {
                   ></IonActionSheet>
                 </IonCol>
               </IonRow>
-              <IonButton
-                expand="block"
-                fill="outline"
-                href="http://www.instagram.com/somd99"
-              >
-                View Policy
-              </IonButton>
+            
 
               <IonButton
                 expand="block"
